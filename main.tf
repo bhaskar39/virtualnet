50da62ce29591e08963540422f0ed8017bb6abf9
Bhaskar Desharaju <bhaskar.desharaju@netenrich.com>
2019-08-03 18:32:30 +0530

    rg name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "aks_vnet" {
   name                  = "${var.project}-${var.environment}-vnet"
   location              = "${var.location}"
-  resource_group_name   = "${var.resouce_group_name}"
+  resource_group_name   = "${var.resource_group_name}"
   address_space         = "${var.vnet_cidr}"
 
   tags {
